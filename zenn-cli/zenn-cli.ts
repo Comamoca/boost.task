a71433af99e3cf6f916e69900d868f91fc165c96
Comamoca <comamoca.dev@gmail.com>
2023-04-01 18:52:50 +0900

    :sparkles: (zenn-cli) Changed the directory structure of tasks
    Change directory structure to match the way it runs in boost

--- zenn-cli/zenn-cli.ts
+++ zenn-cli/zenn-cli.ts
@@ -1,29 +1,29 @@
 import { $ } from "https://deno.land/x/dax@0.27.0/mod.ts";
 
 import {
-	input_eyecatch,
-	input_slug,
-	input_title,
-	input_type,
-} from "./utils.ts";
-import { select } from "./select.ts";
+  input_eyecatch,
+  input_slug,
+  input_title,
+  input_type,
+} from "./zenn-cli/utils.ts";
+import { select } from "./zenn-cli/select.ts";
 
 const task = await select();
 
 if (task === "new:article") {
-	const type = await input_type();
-	const title = await input_title();
-	const slug = await input_slug();
-	const eyecatch = await input_eyecatch();
+  const type = await input_type();
+  const title = await input_title();
+  const slug = await input_slug();
+  const eyecatch = await input_eyecatch();
 
-	console.log(
-		`npx zenn ${task} --slug ${slug} --title ${title} --type ${type} --emoji ${eyecatch}`,
-	);
-	await $`npx zenn ${task} --slug ${slug} --title ${title} --type ${type} --emoji ${eyecatch}`;
+  console.log(
+    `npx zenn ${task} --slug ${slug} --title ${title} --type ${type} --emoji ${eyecatch}`,
+  );
+  await $`npx zenn ${task} --slug ${slug} --title ${title} --type ${type} --emoji ${eyecatch}`;
 } else if (task === "preview") {
-	// await $`npx zenn ${task}`;
-	console.log(`npx zenn ${task}`);
+  // await $`npx zenn ${task}`;
+  console.log(`npx zenn ${task}`);
 } else {
-	console.log("コマンドが見つかりません。");
+  console.log("コマンドが見つかりません。");
 }
 // await $`npx zenn ${task}`;
