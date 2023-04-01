f600a6e932b9b669f84b2f8e4fc94e528108bdcc
Comamoca <comamoca.dev@gmail.com>
2023-04-01 18:50:00 +0900

    :beetle: (zenn-cli) fix to zenn-cli task is not worked

--- zenn-cli/utils.ts
+++ zenn-cli/utils.ts
@@ -3,69 +3,71 @@ import emojiRegex from "npm:emoji-regex";
 import punycode from "npm:punycode";
 
 export async function input_slug(): Promise<string> {
-  const slug = await prompts({
-    type: "text",
-    name: "slug",
-    message: "記事のslugを入力してください。",
-    validate: (value: string) => {
-      const slug_re = new RegExp(/^[a-z0-9_-]{12,50}$/);
+	const slug = await prompts({
+		type: "text",
+		name: "slug",
+		message: "記事のslugを入力してください。",
+		validate: (value: string) => {
+			const slug_re = new RegExp(/^[a-z0-9_-]{12,50}$/);
 
-      if (slug_re.test(value)) {
-        return true;
-      } else {
-        return "slugは半角英数字とハイフン、アンダーバーかつ12～50文字以内である必要があります。";
-      }
-    },
-  });
-  return slug.slug;
+			if (slug_re.test(value)) {
+				return true;
+			} else {
+				return "slugは半角英数字とハイフン、アンダーバーかつ12～50文字以内である必要があります。";
+			}
+		},
+	});
+	return slug.slug;
 }
 
 export async function input_title(): Promise<string> {
-  const title = await prompts({
-    type: "text",
-    name: "title",
-    message: "記事のタイトルを入力してください。",
-  });
+	const title = await prompts({
+		type: "text",
+		name: "title",
+		message: "記事のタイトルを入力してください。",
+	});
 
-  return title.title;
+	return title.title;
 }
 
 export async function input_eyecatch(): Promise<string> {
-  const emoji = await prompts({
-    type: "text",
-    name: "emoji",
-    message: "アイキャッチを指定してください",
-    validate: (value: string) => {
-      const regex = emojiRegex();
+	const emoji = await prompts({
+		type: "text",
+		name: "emoji",
+		message: "アイキャッチを指定してください",
+		validate: (value: string) => {
+			const regex = emojiRegex();
 
-      if (regex.test(value) && (punycode.ucs2.decode(value).length <= 1)) {
-        return true;
-      } else {
-        return "アイキャッチは1文字の絵文字である必要があります。";
-      }
-    },
-  });
+			if (regex.test(value) && (punycode.ucs2.decode(value).length <= 1)) {
+				return true;
+			} else {
+				return "アイキャッチは1文字の絵文字である必要があります。";
+			}
+		},
+	});
 
-  return emoji.emoji;
+	return emoji.emoji;
 }
 
 export async function input_type(): Promise<string> {
-  const emoji = await prompts({
-    type: "select",
-    name: "type",
-    message: "記事のタイプを選択してください。",
-    choices: [
-      {
-        title: "tech",
-        description: "",
-      },
-      {
-        title: "idea",
-        description: "",
-      },
-    ],
-    initial: 1,
-  });
+	const choice = [
+		{
+			title: "tech",
+			description: "",
+		},
+		{
+			title: "idea",
+			description: "",
+		},
+	];
 
-  return emoji.emoji;
+	const type = await prompts({
+		type: "select",
+		name: "type",
+		message: "記事のタイプを選択してください。",
+		choices: choice,
+		initial: 1,
+	});
+
+	return choice[type.type].title;
 }
